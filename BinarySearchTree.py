23cf8cf81d34e6530730ce5d87cc25d68cf5d286
Emre Reyhanlıoğlu <emre.reyhanlioglu@bil.omu.edu.tr>
2013-05-08 13:14:09 +0300

    Create BinarySearchTree.py

---
+++ BinarySearchTree.py
@@ -0,0 +1,54 @@
+from TreeNode import TreeNode
+class BinarySearchTree:
+    def __init__(self):
+        self.root=None
+        self.size=0
+    def put1(self,key,val):
+        if self.root:
+            self.root.put(key,val)
+        else:
+            self.root = TreeNode(key,val)
+        self.size +=1
+    def get(self,key):
+        if self.root:
+            return self.root.get(key)
+        else:
+            return None
+    def __setitem__(self,k,v):
+        self.put1(k,v)
+    def __getitem__(self,key):
+        return self.get(key)
+
+    
+    def hash_key(self,key):
+        if self.root.get(key):
+            return True
+        else:
+            return False
+    def length(self):
+        return self.size
+    def __len__(self):
+        return self.size
+    def delete_key(self,key):
+        if self.size > 1:
+            self.root.delete_key(key)
+            self.size-=1
+        elif self.root.key == key:
+            self.root = None
+            self.size = self.size-1
+        else:
+            print "error ,bad key"
+    
+            
+
+
+#test
+bst=BinarySearchTree()
+bst[70]=70
+bst[31]=31
+bst[93]=93
+bst[94]=94
+bst[14]=14
+bst[23]=23
+bst[73]=73
+
