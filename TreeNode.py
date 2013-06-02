4e529bbc3152fd317e02c50097d4753974acb52b
Emre Reyhanlıoğlu <emre.reyhanlioglu@bil.omu.edu.tr>
2013-06-02 15:07:27 +0300

    Update TreeNode.py

--- TreeNode.py
+++ TreeNode.py
@@ -19,7 +19,8 @@ class TreeNode:
                 self.rightChild = TreeNode(key,val,self)
     def get(self,key):
         if key == self.key:
-            return self.payload
+            print "deger bulundu : ",self.payload
+            return self
         elif key < self.key:
             if self.leftChild:
                 return self.leftChild.get(key)
@@ -33,15 +34,78 @@ class TreeNode:
         else:
             print "error:tihs line should never be executed"
     def delete_key(self,key):
-        if self.key == key:
-            if not(self.leftChild or self.rightChild):
-                if self == self.parent.leftChild:
-                    self.parent.leftChild = None
+        if not (self.leftChild or self.rightChild):
+            print "cocuksuz dugum"
+            if self == self.parent.leftChild:
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif (self.leftChild or self.rightChild) and \
+             (not (self.leftChild and self.rightChild)):
+            if self.leftChild:
+                if self == self.patern.leftChild:
+                    self.parent.leftChild = self.leftChild
                 else:
-                    self.parent.rightChild = None
+                    self.parent.leftChild = self.rightChild
+            else:
+                if self == self.patern.leftChild:
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.leftChild = self.rightChild
+        else:
+            print "iki cocuklu dugum"
+            succ = self.findSuccessor()
+            succ.spliceOut()
+            if self == self.parent.leftChild:
+                self.parent.leftChild = succ
+            else:
+                self.parent.rightChild = succ
+        succ.leftChild = self.leftChild
+        succ.rigthChild = self.rightChild
+        
+                    
+                    
+                        
             
    
     def __setitem__(self,k,v):
         self.put(k,v)
     def __getitem__(self,key):
         return self.get(key)
+
+    def findSuccessor(self):
+        succ = None
+        if self.rightChild:
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent.leftChild == self:
+                succ=self.parent
+            else:
+                self.parent.rightChild = None
+                succ = self.parent.findSuccessor()
+                self.parent.rightChild = self
+        return succ
+    def findMin(self):
+        n= self
+        while n.leftChild:
+            n = n.leftChild
+        print "found min,key = ",n.key
+        return n
+    def spliceOut(self):
+        if (not self.leftChild and not self.rightChild):
+            if self == self.parent.leftChild:
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif (self.leftChild or self.rightChild):
+            if self.leftChild:
+                if self ==self.parent.leftChild:
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.leftChild = self.leftChild
+            else:
+                if self==self.parent.leftChild:
+                    self.parent.leftChild = self.rightChild
+                else:
+                    self.parent.rightChild = self.rightChild
+                
